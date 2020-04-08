8902573c150bd9671df49c57e7e75d06f98996ab
wmarchment <mon@westleymon.com>
2020-04-08 11:20:17 -0700

    ES6 version of find match script

---
+++ find-match-es6.js
@@ -0,0 +1,101 @@
+"use strict";
+
+class MatchFinder {
+    /**
+     * Create a MatchFinder instance
+     * @param element {object} - the element that accepts user input, example html: <input type="text" name="match" id="match-input">
+     * @param box {object} - the element that displays a search suggestion, example html: <div id="suggestion"></div>
+     */
+    constructor(element, box) {
+        if (element && box) {
+            this.element = element;
+            this.suggestionBox = box;
+
+            this.onKeyUp();
+        }
+    }
+
+    onKeyUp() {
+        this.element.addEventListener('onkeyup', (e) => {
+            this.processInput(e.target.value)
+        });
+    }
+
+    /**
+     * Process user input
+     * @param {string} value - the string of user input
+     */
+    processInput(value) {
+        this.getList().then( (terms) => {
+            this.updateSuggestion(value, terms);
+        }).catch((error) => {
+            console.log('Error: ' + error);
+        });
+    }
+
+    getList() {
+        return new Promise(function (resolve) {
+
+            //typically we might kick off some ajax here, but for speediness, lets return some results
+            const list = [
+                'frontend',
+                'backend',
+                'developer',
+                'designer',
+                'ping',
+                'ping pong'
+            ];
+
+            // resolve our promise
+            resolve(list);
+        });
+    }
+
+    /**
+     * Search for a match in the dictionary of terms, and update the suggestion box
+     * @param {string} value - the string of user input
+     * @param termsList {array} - array of terms return by the dictionary
+     */
+    updateSuggestion(value, termsList) {
+        // if input is empty, clear out the suggestion box and exit
+        if (!value) {
+            this.clearSuggestionBox();
+            return;
+        }
+
+        // setting a variable to check against later
+        let noMatchFound = true;
+
+        // declare regex outside of loop for performance gain
+        // testing from beginning of string to match our input value
+        const stringRegExp = new RegExp('^' + value, 'i');
+
+        // loop through list of terms
+        for (let i = 0; i < termsList.length; i++) {
+            const term = termsList[i];
+
+            // test if input string matches a term
+            const matchString = stringRegExp.test(term);
+
+            // regex matches, update suggestion box
+            if (matchString) {
+                noMatchFound = false;
+                this.suggestionBox.innerText = term;
+
+                // we've found a match, so break the loop
+                break;
+            }
+        }
+
+        // if no results, clear out the suggestion box
+        if (noMatchFound) {
+            this.clearSuggestionBox();
+        }
+    }
+
+    clearSuggestionBox() {
+        this.suggestionBox.innerText = '';
+    }
+}
+
+export default MatchFinder;
